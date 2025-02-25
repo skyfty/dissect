c343f897d181b4487ea86b97959534643e9101dc
skyfty <38198234+skyfty@users.noreply.github.com>
2025-02-25 18:05:38 +0800

    using windows global keymaps

--- halve/src/study/Hotkey.h
+++ halve/src/study/Hotkey.h
@@ -1,29 +1,34 @@
 #ifndef HOTKEY_H
 #define HOTKEY_H
+#include <windows.h>
 
 #include <QObject>
 #include <HalveType.h>
 #include <QPointer>
 #include <QShortcut>
 #include <QMap>
+#include <QAbstractNativeEventFilter>
 class Profile;
 
-class Hotkey : public QObject
+class Hotkey : public QObject, public QAbstractNativeEventFilter
 {
     Q_OBJECT
     Q_PROPERTY(Profile* profile WRITE setProfile READ profile NOTIFY profileChanged);
 public:
-    explicit Hotkey(QObject *parent = nullptr);
+    explicit Hotkey(QObject *parent = nullptr);   
+    ~Hotkey();
     void load();
     void load(const QJsonObject &json);
     void setProfile(Profile* profile);
     Profile *profile() const;
 signals:
     void profileChanged();
+protected:
+    bool nativeEventFilter(const QByteArray& eventType, void* message, qintptr* result) override;
 
 private:
     QPointer<Profile> m_profile;
-    QMap<QShortcut*,QString> m_shortcuts;
+    QMap<int,QString> m_shortcuts;
 };
 
 #endif // HOTKEY_H
