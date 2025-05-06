bb2d44c896c62c3b03b289c90f6dfc6df9cc18e7
skyfty <38198234+skyfty@users.noreply.github.com>
2025-05-06 15:53:28 +0800

    提交测试用例

--- halve/tests/tst_catheter_perception.cpp
+++ halve/tests/tst_catheter_perception.cpp
@@ -111,66 +111,20 @@ using namespace Eigen;
 //    // 最小二乘法求解权重矩阵
 //    W = (X_poly.transpose() * X_poly).ldlt().solve(X_poly.transpose() * y);
 //}
-
-void TestCatheterPerception::test1() {
-    // 训练数据
-
-	vtkNew<CatheterPerception> cp;
-    cp->setMode(CatheterPerception::PREDICT);
-	cp->addSpline(0);
-	cp->addSpline(1);
-	cp->addSpline(2);
-
-	std::vector<std::vector<vtkVector3d>> du{
-		{{ -0.625, -1.08, 15.5},{ -0.625, -1.08, 28},{ -0.625, -1.08, 12.5}},
-		{{-2.55, -4.4, 15},{ -1.25, -2.17, 27.24},{ -2.2, -3.8, 12.1}},
-		{{ -2.75, -4.76, 14.84 },{ -1.35, -2.34, 27 },{-2.6125, -4.525, 11.85}},
-		{{ -4.415, -7.65, 13.42},{ -1.5875, -2.75, 24.57 },{ -3.975, -6.88, 10.55}},
-		{{ -5.15, -8.92, 12.458},{-2, -3.46, 23.25},{-4.575, -7.924, 9.69}},
-		//{{-5.15, -8.92, 12.458},{-2, -3.46, 23.25},{-4.575, -7.924, 9.69}},
-		{{-6.1875, -10.7, 10},{-2.375, -4.11, 19.9},{-5.75, -9.96, 7.15}},
-		{{-6.75, -11.69, 7.47},{-2.5, -4.33, 16.64},{-6.45, -11.17, 4.53}},
-		{{-7.15, -12.38, 7},{-2.625, -4.55, 15.6},{-6.5, -11.25, 4.26}},
-		{{-7.5, -12.99, 3.8},{-2.75, -4.76, 11.9},{-6.85, -11.86, 1.12}},
-	};
-	// 设置一些数据
-	cp->setDu(du);
-	cp->train();
-
-	{
-		vtkNew<vtkPoints> points;
-		points->InsertNextPoint(-1.25, -2.17, 27.24);
-		points->InsertNextPoint(-2.2, -3.8, 12.1);
-
-		vtkVector3d r;
-		cp->predict(points, r);
-		qDebug() << "Predicted point:" << r.GetX() << r.GetY() << r.GetZ();
-
-	}
-
-
-}
 //
 //void TestCatheterPerception::test1() {
-//	// 训练数据
+//    // 训练数据
 //
 //	vtkNew<CatheterPerception> cp;
-//	cp->setMode(CatheterPerception::EXPLICIT);
+//    cp->setMode(CatheterPerception::PREDICT);
 //	cp->addSpline(0);
 //	cp->addSpline(1);
 //	cp->addSpline(2);
 //
 //	std::vector<std::vector<vtkVector3d>> du{
-//		{{ 1.25, 0, 15.5},{ 1.25, 0, 28},{  1.25, 0, 12.5}},
-//		{{5.1, 0, 15},{  2.5, 0, 27.24 },{4.4, 0, 12.1}},
-//		{{5.5, 0, 14.84 },{  2.7, 0, 27 },{5.225, 0, 11.85}},
-//		{{8.83, 0, 13.42},{3.175, 0, 24.57 },{ 7.95, 0, 10.55}},
-//		{{10.3, 0, 12.458},{4, 0, 23.25},{9.15, 0, 9.69}},
-//		//{{10.3, 0, 12.458},{ 4, 0, 23.25},{9.15, 0, 9.69}},
-//		{{12.375, 0, 10},{4.75, 0, 19.9},{ 11.5, 0, 7.15}},
-//		{{ 13.5, 0, 7.47},{5, 0, 16.64},{12.9, 0, 4.53 }},
-//		{{ 14.3, 0, 7},{5.25, 0, 15.6},{ 13, 0, 4.26}},
-//		{{15, 0, 3.8},{ 5.5, 0, 11.9},{13.7, 0, 1.12}},
+//		{{1, 0.5, 0},{0, 0, 0},{2, 0, 0}},
+//		{{2, 0.7, 0},{1, 0, 0},{3, 0, 0}},
+//		{{0, 1, 0.5},{0, 0, 0},{0, 2, 0}},
 //	};
 //	// 设置一些数据
 //	cp->setDu(du);
@@ -178,11 +132,9 @@ void TestCatheterPerception::test1() {
 //
 //	{
 //		vtkNew<vtkPoints> points;
-//		//points->InsertNextPoint(23.877140045166016, 18.603031158447266, 49.284667968750000);
-//		//points->InsertNextPoint(23.242828369140625, 11.295700073242188, 38.926269531250000);
-//
-//		points->InsertNextPoint(1.25, 0, 28);
-//		points->InsertNextPoint(1.25, 0, 12.5);
+//		points->InsertNextPoint(0,0, 0);
+//		points->InsertNextPoint(1, 1, 0);
+//		points->InsertNextPoint(3, 1, 0);
 //
 //		vtkVector3d r;
 //		cp->predict(points, r);
@@ -192,6 +144,48 @@ void TestCatheterPerception::test1() {
 //
 //
 //}
+//
+void TestCatheterPerception::test1() {
+	// 训练数据
+
+	vtkNew<CatheterPerception> cp;
+	cp->setMode(CatheterPerception::EXPLICIT);
+	cp->addSpline(0);
+	cp->addSpline(1);
+	cp->addSpline(2);
+
+	std::vector<std::vector<vtkVector3d>> du{
+		{{ 1.25, 0, 15.5},{ 1.25, 0, 28},{  1.25, 0, 12.5}},
+		{{5.1, 0, 15},{  2.5, 0, 27.24 },{4.4, 0, 12.1}},
+		{{5.5, 0, 14.84 },{  2.7, 0, 27 },{5.225, 0, 11.85}},
+		{{8.83, 0, 13.42},{3.175, 0, 24.57 },{ 7.95, 0, 10.55}},
+		{{10.3, 0, 12.458},{4, 0, 23.25},{9.15, 0, 9.69}},
+		//{{10.3, 0, 12.458},{ 4, 0, 23.25},{9.15, 0, 9.69}},
+		{{12.375, 0, 10},{4.75, 0, 19.9},{ 11.5, 0, 7.15}},
+		//{{ 13.5, 0, 7.47},{5, 0, 16.64},{12.9, 0, 4.53 }},
+		{{ 14.3, 0, 7},{5.25, 0, 15.6},{ 13, 0, 4.26}},
+		{{15, 0, 3.8},{ 5.5, 0, 11.9},{13.7, 0, 1.12}},
+	};
+	// 设置一些数据
+	cp->setDu(du);
+	cp->train();
+
+	{
+		vtkNew<vtkPoints> points;
+		//points->InsertNextPoint(23.877140045166016, 18.603031158447266, 49.284667968750000);
+		//points->InsertNextPoint(23.242828369140625, 11.295700073242188, 38.926269531250000);
+		points->InsertNextPoint(0, 0, 0);
+		points->InsertNextPoint(5, 0, 16.64);
+		points->InsertNextPoint(12.9, 0, 4.53);
+
+		vtkVector3d r;
+		cp->predict(points, r);
+		qDebug() << "Predicted point:" << r.GetX() << r.GetY() << r.GetZ();
+
+	}
+
+
+}
 
 QTEST_MAIN(TestCatheterPerception)
 #include "tst_catheter_perception.moc"
